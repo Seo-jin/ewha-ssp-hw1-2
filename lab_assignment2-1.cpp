0104ad568ed8c62dd3748a2789c6935627f9a976
Seo-jin <[aldebaran1583@gmail.com]>
2015-03-27 02:08:18 +0900

    cpp were modified

--- lab_assignment2-1.cpp
+++ lab_assignment2-1.cpp
@@ -4,42 +4,49 @@
 
 #define FRONT_LED_PIN 10
 #define REAR_LED_PIN 9
+#define DUTY_CYCLE 20 //20ms
 
+int done = false;
 
+void PWM_Write(int pin, int on_time_perc)
+{
+	int on_time = DUTY_CYCLE*on_time_perc/100.0;
+	int off_time = DUTY_CYCLE - on_time;
+	digitalWrite(pin, HIGH);
+	delay(on_time);
+	digitalWrite(pin, LOW);
+	delay(off_time);
+}
+void myAnalogWrite(int pin, int percent, int time)
+{
+	int num_loops = time/DUTY_CYCLE;
+	int i;
+	for (i=0; i<num_loops; i++)
+	{
+		PWM_Write(pin, percent);
+	}
+}
 //The setup function is called once at startup of the sketch
 void setup()
 {
-	pinMode(FRONT_LED_PIN, OUTPUT);
 	pinMode(REAR_LED_PIN, OUTPUT);
-
-	digitalWrite(FRONT_LED_PIN, LOW);
-	digitalWrite(REAR_LED_PIN, HIGH);
 }
-
 // The loop function is called in an endless loop
 void loop()
 {
-	int front_brightness=0;
-	int rear_brightness=255;
-	int brightness_change=51;
-
-	int i=0;
-
-	for(i=0;i<5;i++){
-		front_brightness=front_brightness+brightness_change;
-		rear_brightness=rear_brightness-brightness_change;
-		analogWrite(FRONT_LED_PIN, front_brightness);
-		analogWrite(REAR_LED_PIN, rear_brightness);
-		delay(1000);
-
+	if (done == false)
+	{
+		int value = 100;
+		int value_interval = 5;
+		int total_duration = 10000; //10,000ms
+		int time_per_step = total_duration/(value/value_interval);
+		while (value > 0)
+		{
+			myAnalogWrite(REAR_LED_PIN, value, time_per_step);
+			value -= value_interval;
+		}
+		done = true;
 	}
-
-	for(i=0;i<5;i++){
-		front_brightness=front_brightness-brightness_change;
-		rear_brightness=rear_brightness+brightness_change;
-		analogWrite(FRONT_LED_PIN, front_brightness);
-		analogWrite(REAR_LED_PIN, rear_brightness);
-		delay(1000);
-	}
-
+	else
+		digitalWrite(REAR_LED_PIN, LOW);
 }
